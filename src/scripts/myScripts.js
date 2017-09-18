b48c5265050dc297125f8b6dc0e24bf8ca3b6b49
db001 <dnbennett@hotmail.co.uk>
2017-09-18 13:14:24 +0100

    Adding function to create API URL from form

--- src/scripts/myScripts.js
+++ src/scripts/myScripts.js
@@ -1,14 +1,43 @@
-const API_KEY = `${removed}`;
+const API_KEY = `Removed`;
+
+const genres = [
+    {"id": 28, "name": "Action"},
+    {"id": 12, "name": "Adventure"},
+    {"id": 16, "name": "Animation"},
+    {"id": 35, "name": "Comedy"},
+    {"id": 80, "name": "Crime"},
+    {"id": 99, "name": "Documentary"},
+    {"id": 18, "name": "Drama"},
+    {"id": 10751, "name": "Family"},
+    {"id": 14, "name": "Fantasy"},
+    {"id": 36, "name": "History"},
+    {"id": 27, "name": "Horror"},
+    {"id": 10402, "name": "Music"},
+    {"id": 9648, "name": "Mystery"},
+    {"id": 10749, "name": "Romance"},
+    {"id": 878, "name": "Science Fiction"},
+    {"id": 10770, "name": "TV Movie"},
+    {"id": 53, "name": "Thriller"},
+    {"id": 10752, "name": "War"},
+    {"id": 37, "name": "Western"}
+];
 
 function createUrl() {
   let baseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&region=GB&sort_by=popularity.desc`;
 
   let cert = `&certification=${document.getElementById('js-cert').value}`;
-  let genre = document.getElementById('js-genre').value;
+  let genreURL = `with_genres=${getGenre(genres, document.getElementById('js-genre').value)}`;
   let relYear = `primary_release_year=${document.getElementById('js-releaseYear').value}`;
 
-  return `${baseURL}&certification_country=GB${cert}&include_video=false&page=1&${relYear}` 
+  let URL = `${baseURL}&certification_country=GB${cert}&include_video=false&page=1&${relYear}&${genreURL}` 
 
+  console.log(URL);
+  return URL;
+
+}
+
+function getGenre(arr, genre) {
+  return arr.filter(obj => obj.genre);
 }
 
 let movieURL = createUrl();
